ba5f1597837932d970704bec3e7882a9b9ab429d
Ehsan Davoudi <ehsandavoudi370@gmail.com>
2022-08-07 10:40:59 +0430

    Update ostovane.py

--- ostovane.py
+++ ostovane.py
@@ -3,7 +3,7 @@ r = float(input('shoaae ra vared konid: '))
 h = float(input('ertefa ra vared konid: '))
 masahat_j  = 2 * pi * r * h
 hajm = pi * (r ** 2) * h
-masahat_k = masahat_j + 2 * pi * (r ** 2)
+masahat_k = (masahat_j) + 2 * pi * (r ** 2)
 print('masahat janebi ostovane: ', masahat_j)
 print('hajm ostovane:', hajm)
 print('masahat koli ostovane: ', masahat_k)
