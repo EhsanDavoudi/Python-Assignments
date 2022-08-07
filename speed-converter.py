c7524769268b10a300c415e01bbe967754624b35
Ehsan Davoudi <ehsandavoudi370@gmail.com>
2022-08-07 10:39:50 +0430

    Add files via upload

---
+++ speed-converter.py
@@ -0,0 +1,3 @@
+s_m = float(input('sorat mashin ra be mph vared konid: '))
+s_km = s_m * 1.60934
+print('sorat mashin be km/h: ', s_km)
---
+++ sum-number.py
@@ -0,0 +1,5 @@
+n = int(input('adadi 6raghami vared konid: '))
+fifth_num = (n // 10) % 10
+second_num = (n // 10000) % 10
+sum = fifth_num + second_num
+print('majmoe ragham panjom va dovvom: ', sum)
