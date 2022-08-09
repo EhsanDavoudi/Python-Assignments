864852cbe2b33b3419d7281d53d3eab5acd28497
Ehsan Davoudi <ehsandavoudi370@gmail.com>
2022-08-09 12:33:23 +0430

    Update sum-number.py

--- sum-number.py
+++ sum-number.py
@@ -1,5 +1,5 @@
-n = int(input('adadi 6raghami vared konid: '))
-fifth_num = (n // 10) % 10
-second_num = (n // 10000) % 10
+num = int(input('adadi 6raghami vared konid: '))
+fifth_num = (num // 10) % 10
+second_num = (num // 10000) % 10
 sum = fifth_num + second_num
 print('majmoe ragham panjom va dovvom: ', sum)
