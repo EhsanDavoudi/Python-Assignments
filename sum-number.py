a658c4ef42dbe941a8d3ea1af8690449c8831f26
Ehsan Davoudi <ehsandavoudi370@gmail.com>
2022-08-14 19:13:25 +0430

    Delete sum-number.py

--- sum-number.py
+++
@@ -1,5 +0,0 @@
-num = int(input('adadi 6raghami vared konid: '))
-fifth_num = (num // 10) % 10
-second_num = (num // 10000) % 10
-sum = fifth_num + second_num
-print('majmoe ragham panjom va dovvom: ', sum)
