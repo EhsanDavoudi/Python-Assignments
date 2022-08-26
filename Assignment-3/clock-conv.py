9b9f45aa17ebdd676546d5b480df9ae531db661a
Ehsan Davoudi <ehsandavoudi370@gmail.com>
2022-08-26 15:43:47 +0430

    Update clock-conv.py

--- Assignment-3/clock-conv.py
+++ Assignment-3/clock-conv.py
@@ -1,14 +1,19 @@
-tp = int(input('1.hour to seconds\n2.seconds to hour\n1 or 2: '))
-
-if tp == 1:
-    hr = int(input('hours: '))
-    min = int(input('minutes: '))
-    sec = int(input('seconds: '))
-    secs = (hr * 3600) + (min * 60) + sec
-    print(secs, 'seconds')
-elif tp == 2:
-    secs = int(input('seconds: '))
-    hr = int(secs / 3600)
-    min = int((secs - (hr * 3600)) / 60)
-    sec = int(((secs - (hr * 3600) - min * 60) / 60) * 60)
-    print(hr,':', min,':', sec)
+while True:
+    tp = int(input('1.hour to seconds\n2.seconds to hour\n1 or 2: '))
+    if tp == 1:
+        hr = int(input('hours: '))
+        min = int(input('minutes: '))
+        sec = int(input('seconds: '))
+        secs = (hr * 3600) + (min * 60) + sec
+        print(secs, 'seconds')
+        break
+    elif tp == 2:
+        secs = int(input('seconds: '))
+        hr = int(secs / 3600)
+        min = int((secs - (hr * 3600)) / 60)
+        sec = int(((secs - (hr * 3600) - min * 60) / 60) * 60)
+        print(hr,':', min,':', sec)
+        break
+    else:
+        print('you have to Enter "1" or "2"')
+        continue
